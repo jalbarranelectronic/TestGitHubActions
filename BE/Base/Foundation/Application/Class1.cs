d65eccfcbe00379863a28583d008b1e4520ec75d
Albarran, Jorge <j.albarran@controlexpert.com>
2024-02-19 14:42:42 -0600

    Add a `DoSomething` method

--- BE/Base/Foundation/Application/Class1.cs
+++ BE/Base/Foundation/Application/Class1.cs
@@ -6,5 +6,12 @@
         int x = 0;
         int y = 0;
         int z = 0;
+
+        public void DoSomething()
+        {
+            x = 1;
+            y = 2;
+            z = 3;
+        }
     }
 }
